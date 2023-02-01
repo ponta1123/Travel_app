95b7dbcb5d55fbdcf02d854819a9398043edbf92
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2023-02-01 13:19:52 +0000

    [add]admin,userをdeviseで追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  #管理者　URL /admin/sign=in...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+  #ユーザー URL /users/sign_in....
+  devise_for :users,skip: [:passwords] ,controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
 end
